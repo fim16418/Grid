6b783665ed94b2554be18a1557925ca92c81fc03
Moritz <moritz.fink@physik.uni-r.de>
2017-04-25 15:45:39 +0200

    Add files via upload

--- Benchmark_su3.cpp
+++ Benchmark_su3.cpp
@@ -49,6 +49,7 @@ using namespace Grid::QCD;
 
 int nLoops;
 std::vector<int> latt_size(4);
+std::vector<int> mpi_layout(4);
 int nThreads;
 std::string outFileName;
 
@@ -57,6 +58,7 @@ bool processCmdLineArgs(int argc,char** argv)
   nLoops = 1000;
   nThreads = omp_get_max_threads();
   outFileName = "output.txt";
+  mpi_layout = {1,1,1,1};
 
   for(int i=1; i<argc; i++) {
     std::string option = std::string(argv[i]);
@@ -70,7 +72,6 @@ bool processCmdLineArgs(int argc,char** argv)
     } else if(option == "--nThreads") {
       if(i+1 < argc) {
         nThreads = atoi(argv[++i]);
-        //omp_set_num_threads(nThreads);
         GridThread::SetThreads(nThreads);
       } else {
         std::cerr << "--nThreads option requires one argument." << std::endl;
@@ -83,10 +84,21 @@ bool processCmdLineArgs(int argc,char** argv)
         std::cerr << "--outFile option requires one argument." << std::endl;
         return false;
       }
+    } else if(option == "--mpiLayout") {
+      if(i+4 < argc) {
+        for(int j=0; j<4; j++) {
+          mpi_layout[j] = atoi(argv[i+j+1]);
+        }
+        i+=4;
+      } else {
+        std::cerr << "--mpiLayout option requires four arguments." << std::endl;
+        return false;
+      }
     }
   }
   std::cout << "Loops per measurement = " << nLoops << std::endl
             << "Threads = " << omp_get_max_threads() << std::endl
+            << "Mpi Layout = " << mpi_layout[0] << " " << mpi_layout[1] << " " << mpi_layout[2] << " " << mpi_layout[3] << std::endl
             << "Output file = " << outFileName << std::endl << std::endl;
   return true;
 }
@@ -99,7 +111,6 @@ int main (int argc, char ** argv)
   }
 
   std::vector<int> simd_layout = GridDefaultSimd(Nd,vComplex::Nsimd());
-  std::vector<int> mpi_layout  = GridDefaultMpi();
 
   int threads = GridThread::GetThreads();
   std::cout<<GridLogMessage << "Grid is setup to use "<<threads<<" threads"<<std::endl;
@@ -109,7 +120,6 @@ int main (int argc, char ** argv)
     latt_size = {lat*mpi_layout[0],lat*mpi_layout[1],lat*mpi_layout[2],lat*mpi_layout[3]};
     int vol = latt_size[0]*latt_size[1]*latt_size[2]*latt_size[3];
     GridCartesian     Grid(latt_size,simd_layout,mpi_layout);
-    //      GridParallelRNG          pRNG(&Grid);      pRNG.SeedRandomDevice();
 
     LatticeColourMatrix z(&Grid);// random(pRNG,z);
     LatticeColourMatrix x(&Grid);// random(pRNG,x);
@@ -125,13 +135,24 @@ int main (int argc, char ** argv)
     double bytes=3.0*vol*Nc*Nc*sizeof(Complex);
     double flops=Nc*Nc*(6.0+8.0+8.0)*vol;
 
-    ofstream file;
-    file.open(outFileName,ios::app);
-    if(file.is_open()) {
-      file << threads << "\t" << latt_size[0] << "\t" << bytes << "\t" << bytes/time << "\t" << flops/time << std::endl;
-      file.close();
-    } else {
-      std::cerr << "Unable to open file!" << std::endl;
+    int nProc = Grid.ProcessorCount();
+    int rank = Grid.RankWorld();
+
+    Grid.Barrier();
+
+    double sumTime;
+    MPI_Reduce(&time,&sumTime,1,MPI_DOUBLE,MPI_SUM,Grid.BossRank(),Grid.communicator);
+    time = sumTime/nProc;
+
+    if(Grid.IsBoss()) {
+      ofstream file;
+      file.open(outFileName,ios::app);
+      if(file.is_open()) {
+        file << threads << "\t" << latt_size[0] << "\t" << bytes << "\t" << bytes/time << "\t" << flops/time << std::endl;
+        file.close();
+      } else {
+        std::cerr << "Unable to open file!" << std::endl;
+      }
     }
   }
 
