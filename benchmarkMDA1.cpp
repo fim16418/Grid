2812d70d275504ca136782ff2e80a05678775e8c
Moritz <moritz.fink@physik.uni-r.de>
2017-06-12 15:12:23 +0200

    Add files via upload

--- benchmarkMDA1.cpp
+++ benchmarkMDA1.cpp
@@ -137,7 +137,7 @@ int main (int argc, char ** argv)
 
   GridParallelRNG rng(&Grid);
   //rng.SeedRandomDevice();
-  rng.SeedFixedIntegers(std::vector<int>({1,2,3,4});
+  rng.SeedFixedIntegers(std::vector<int>({1,2,3,4}));
 
   int vol = latt_size[0]*latt_size[1]*latt_size[2]*latt_size[3];
 
